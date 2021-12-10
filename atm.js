f60090da30e30c8cd3ec29311bc00b2341800114
Selvin Iraheta <irahetaselvin9@gmail.com>
2021-12-09 19:48:16 -0500

    Finished ATM

--- atm.js
+++ atm.js
@@ -1,5 +1,5 @@
 let {balance,pin}= require("./account")
-let {pocketbalance}=require("/.wallet")
+let {pocketBalance}=require("/.wallet")
 
 const prompt = require ("prompt-sync")();
 
@@ -10,10 +10,31 @@ function getBalance(){
 
 function withdraw (){
     let amount= prompt ("How much would you like to withdraw?")
+
+    balance=parseFloat(balance) - parseFloat (amount);
+    pocketBalance=parseFloat(pocketBalance) - parseFloat(amount);
+    console.log("Your Balance:"+ pocketBalance);
 }
 
-function deposit ()
+function deposit (){
+    let amount=prompt ("Enter deposited amount:");
 
-function validatePin ()
+    balance = parseFloat(balance) + parseFloat (amount);
+    pocketBalance = parseFloat(pocketBalance) - parseFloat (amount);
+    console.log("Your Balance:"+ pocketBalance)
+}
 
+function validatePin (){
+    let userPin= prompt('Please enter your pin');
+if (parseInt(userPin)=== pin){
+    return true;}
+    else {console.log("Incorrect Pin");
+    return false;}
+}
 
+module.exports={
+    getBalance: getBalance,
+    withdraw: withdraw,
+    deposit: deposit,
+    validatePin: validatePin
+}
